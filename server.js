c5faeb86807987a5cc2bb84b5d2cb9dea67bf6c4
Pranavpatell <89290846+Pranavpatell@users.noreply.github.com>
2025-04-14 09:48:27 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index3.html');
+  });
+  
+mongoose.connect(process.env.MONGO_URI || 'mongodb://mongo:YKdhFxNCBVpvWuotHDCKHvwAKdRjxOuT@trolley.proxy.rlwy.net:35483/');
+
+const FormSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  
+  message: String
+});
+
+const Form = mongoose.model('Form', FormSchema);
+
+app.post('/submit', async (req, res) => {
+  try {
+    const form = new Form(req.body);
+    await form.save();
+    res.send('Form Submitted Successfully!');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Failed to submit form');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
