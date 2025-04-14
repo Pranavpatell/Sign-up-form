21d95b9b5e965f64da3a43e67012148c5080eae1
Pranavpatell <89290846+Pranavpatell@users.noreply.github.com>
2025-04-14 10:40:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.post('/submit', async (req, res) => {
 });
 
 // 🚀 Dynamic port for Railway
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
